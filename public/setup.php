54e35596c8312cef663c0f2356436aa8a3fde15b
Marcos Gurgel <mgurgel@gmail.com>
2015-02-24 12:19:56 +0000

    Setup script

---
+++ public/setup.php
@@ -0,0 +1,57 @@
+<?php
+ 
+  // Set default timezone
+  date_default_timezone_set('UTC');
+ 
+  try {
+    /**************************************
+    * Create databases and                *
+    * open connections                    *
+    **************************************/
+ 
+    // Create (connect to) SQLite database in file
+    $file_db = new PDO('sqlite:colour.sqlite3');
+    // Set errormode to exceptions
+    $file_db->setAttribute(PDO::ATTR_ERRMODE, 
+                            PDO::ERRMODE_EXCEPTION);
+ 
+
+    /**************************************
+    * Create tables                       *
+    **************************************/
+ 
+    // Create table messages
+    $file_db->exec("CREATE TABLE IF NOT EXISTS albums (
+                    id INTEGER PRIMARY KEY, 
+                    title TEXT, 
+                    artist_name TEXT,
+                    artist_id INTEGER,
+                    release_date TEXT,
+                    mbid TEXT,
+                    image_url TEXT,
+                    hex_value TEXT)");
+ 
+    $file_db->exec("CREATE TABLE IF NOT EXISTS artists (
+                    id INTEGER PRIMARY KEY, 
+                    name TEXT, 
+                    mbid TEXT)");
+
+    $file_db->exec("CREATE TABLE IF NOT EXISTS genres (
+                    id INTEGER PRIMARY KEY, 
+                    tite TEXT, 
+                    blurb TEXT)");
+
+    $file_db->exec("CREATE TABLE IF NOT EXISTS albums_artists (
+                    id INTEGER PRIMARY KEY, 
+                    album_id INT, 
+                    artist_id INT)");
+
+   // Close file db connection
+    $file_db = null;
+  }
+  catch(PDOException $e) {
+    // Print PDOException message
+    echo $e->getMessage();
+  }
+?>
+ 
